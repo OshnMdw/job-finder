419958407cb2309571e31cd70822556332c2c38d
OshnMdw <gchoshanmadawa@gmail.com>
2023-10-01 20:08:58 +0530

    api folder

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ID: "d4bf1a7d",
+  API_KEY: "8c5dd76434a6c3033362aa8af2852d0b",
+  BASE_URL: "https://api.adzuna.com/v1/api/jobs",
+  BASE_PARAMS: "search/1?&results_per_page=20&content-type=application/json",
+};
